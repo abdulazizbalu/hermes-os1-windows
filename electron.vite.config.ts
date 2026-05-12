5506976c8cfe311185bc208fa67f84d826665dbf
Abdulaziz <Abdushkin2001@gmail.com>
2026-05-12 17:03:05 +0500

    chore: scaffold Electron React tooling

---
+++ electron.vite.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig, externalizeDepsPlugin } from "electron-vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "node:path";
+
+export default defineConfig({
+  main: {
+    plugins: [externalizeDepsPlugin()],
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, "src/main/main.ts")
+        }
+      }
+    }
+  },
+  preload: {
+    plugins: [externalizeDepsPlugin()],
+    build: {
+      rollupOptions: {
+        input: {
+          index: resolve(__dirname, "src/preload/index.ts")
+        }
+      }
+    }
+  },
+  renderer: {
+    plugins: [react()],
+    build: {
+      rollupOptions: {
+        input: {
+          index: resolve(__dirname, "index.html")
+        }
+      }
+    }
+  }
+});
